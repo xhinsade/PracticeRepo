2f5c51c6c469b4625e9d4f4d632b6cbb5a7e97f4
xhinsade <nekochii57@gmail.com>
2024-08-30 14:17:53 +0800

    Practice Javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+    
+    const person = {
+        name: "Kristine Lopez",
+        yearlevel: "2nd Year ",
+        Course: "Object Oriented Programming",
+        address: "Purok 4, San Miguel, Manolo Fortich Bukidnon",
+        age: 20,
+        gender: "female",
+        hobby: "Arts",
+        mood: "tired but irritated",
+    }
+    
+
+console.log(person);
